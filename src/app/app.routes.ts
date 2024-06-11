ca283aa7ea8ae48362d5ec34b86525009eed97d3
Reggie Basinal <34482113+gbasinal@users.noreply.github.com>
2024-06-11 15:16:38 -0600

    added base folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { DetailsComponent } from './features/details/details.component';
+import { SearchComponent } from './features/search/search.component';
+import { UserComponent } from './features/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "", component : HomeComponent},
+    {path : "details/:id", component : DetailsComponent},
+    {path : "search", component : SearchComponent},
+    {path : "user", component : UserComponent},
+];
